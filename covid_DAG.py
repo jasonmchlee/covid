8a4116f00f737cb7e4bd47434ee1effc2b5f59db
Jason Lee <jasonmchlee@gmail.com>
2020-10-27 21:08:36 -0700

    updates

--- covid_DAG.py
+++ covid_DAG.py
@@ -1,10 +1,9 @@
 from airflow.models import DAG
 from datetime import datetime, timedelta
-from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 #
 # Import functions
-from covid_datasets import world_covid_data
+from covid_datasets import covid_data, covid_pivots, mortality_data
 from usa_covid_cases import usa_covid_data
 
 
@@ -16,17 +15,27 @@ default_args = {
 
 dag =  DAG(dag_id = 'covid_updates',
            default_args = default_args,
-           schedule_interval = "0 4 * * *")
+           schedule_interval = "30 4 * * *") # 8:30 pm daily
 
-# World datasets
+# Download WHO dataset
 t1 = PythonOperator(task_id = 'world_covid_data',
                     python_callable = world_covid_data,
                     dag = dag)
 
+# Save pivot tables from WHO dataset
+t2 = PythonOperator(task_id = 'covid_pivots',
+                    python_callable = covid_pivots,
+                    dag = dag)
+
+# Mortality rate data
+t3 = PythonOperator(task_id = 'mortality_data',
+                    python_callable = mortality_data,
+                    dag = dag)
+
 # USA datasets
-t2 = PythonOperator(task_id = 'usa_covid_data',
+t4 = PythonOperator(task_id = 'usa_covid_data',
                     python_callable = usa_covid_data,
                     dag = dag)
 
 # Dependecies
-[t1, t2]
+t1 >> [t2,t3] >> t4
--- covid_datasets.py
+++ covid_datasets.py
@@ -5,33 +5,80 @@
 import pandas as pd
 import datetime
 
-def world_covid_data():
 
 
-    # Reading URL
+# Read data from WHO website
+def covid_data():
+
     url = 'https://covid19.who.int/WHO-COVID-19-global-data.csv'
     df = pd.read_csv(url)
+    df.to_csv('~/files_airflow/WHO-COVID-19-global-data.csv')
+
+
+
+
+# Converting tables into pivot tables
+def covid_pivots():
+
+    # Reading URL
+    file = '~/files_airflow/WHO-COVID-19-global-data.csv'
+    df = pd.read_csv(file)
 
     # Converting to datetime
-    df['Date_reported'] = pd.to_datetime(df['Date_reported'])
+    df['Date_reported'] = pd.to_datetime(df['Date_reported']).dt.date.astype('datetime64')
 
     # Cleaning column names for trailing white spaces
     cols = []
     for col in df.columns:
-        cols.append(col.strip())
+        cols.append(col.strip().lower())
 
     df.columns = cols
 
     # New col and filling for 0 values
-    df['Mortality_rate'] = ((df['Cumulative_deaths']/df['Cumulative_cases'])*100).round(2).fillna(0)
+    df['mortality_rate'] = ((df['cumulative_deaths']/df['cumulative_cases'])*100).round(2).fillna(0)
 
 
     # Saving .csvs
-    columns = ['New_cases', 'Cumulative_cases', 'New_deaths', 'Cumulative_deaths', 'Mortality_rate']
+    columns = ['new_cases', 'cumulative_cases', 'new_deaths', 'cumulative_deaths', 'mortality_rate']
 
     for col in columns:
-        dfs = df.pivot_table(index = 'Country', columns = 'Date_reported', values = col)
+        dfs = df.pivot_table(index = 'country', columns = 'date_reported', values = col)
         dfs.to_csv('~/files_airflow/covid_report_{0}.csv'.format(col))
 
 
-# In[ ]:
+
+
+# Dataset for most current data case, deaths and mortality_rate
+def mortality_data():
+
+    file = '~/files_airflow/WHO-COVID-19-global-data.csv'
+    df = pd.read_csv(file)
+
+    # Remove unwated column
+    for col in df.columns:
+        if col == 'Unnamed: 0':
+            df.drop('Unnamed: 0', axis = 1, inplace = True)
+
+    # Clean column name
+    cols = []
+
+    for col in df.columns:
+        clean = col.strip().lower()
+        cols.append(clean)
+
+    df.columns = cols
+
+    # Convert to datetime
+    df['date_reported'] = pd.to_datetime(df['date_reported']).dt.date.astype('datetime64')
+
+    # Recent data
+    recent_date = max(df['date_reported'].dt.date)
+    recent_data = df[df['date_reported'] == recent_date].reset_index(drop = True)
+
+    # Saving relevant_data
+    cols = ['country', 'cumulative_cases', 'cumulative_deaths']
+    relevant_data = recent_data.loc[:, cols]
+    relevant_data['mortality_rate'] = ((relevant_data['cumulative_deaths']/relevant_data['cumulative_cases']) * 100).round(2).fillna(0)
+    mortality_rate_data = relevant_data.sort_values('mortality_rate', ascending = False).reset_index(drop = True)
+
+    mortality_rate_data.to_csv('~/files_airflow/mortality_rate_data.csv')
